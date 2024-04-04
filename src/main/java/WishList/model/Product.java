9a2b1140f7cee26259fe95d36ad01b5b7e7053ac
LasseThomsenSkole <lath0003@stud.kea.dk>
2024-04-04 11:12:50 +0200

    product getter - setter

--- src/main/java/WishList/model/Product.java
+++ src/main/java/WishList/model/Product.java
@@ -5,6 +5,7 @@ public class Product {
     private String description;
     private double price;
     private String url;
+
     public Product(String name, String description, double price, String url){
         this.name = name;
         this.description = description;
