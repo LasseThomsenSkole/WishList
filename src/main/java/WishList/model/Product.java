da0a1bcdfa566690caf289a91bcee3a4b6ed3462
LasseThomsenSkole <lath0003@stud.kea.dk>
2024-04-04 11:11:20 +0200

    product getter - setter

--- src/main/java/WishList/model/Product.java
+++ src/main/java/WishList/model/Product.java
@@ -12,4 +12,35 @@ public class Product {
         this.url = url;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
