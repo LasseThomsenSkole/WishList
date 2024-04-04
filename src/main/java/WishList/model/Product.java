76be0822d0972480034ef698b03015a8e9e65317
LasseThomsenSkole <lath0003@stud.kea.dk>
2024-04-04 11:10:46 +0200

    product constructor

--- src/main/java/WishList/model/Product.java
+++ src/main/java/WishList/model/Product.java
@@ -1,8 +1,15 @@
 package WishList.model;
 //SKAL MÅSKE VÆRE ET ANDET STED IDK
 public class Product {
-    private String Name;
-    private String Description;
+    private String name;
+    private String description;
     private double price;
-    private String Url;
+    private String url;
+    public Product(String name, String description, double price, String url){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.url = url;
+    }
+
 }
