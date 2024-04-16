c26b85dad5da1820f6c47dc6fcfcca03c8de2314
hantamil <mira0004@stud.kea.dk>
2024-04-16 14:17:23 +0200

    test

---
+++ src/test/java/WishList/WishlistJDBCIT.java
@@ -0,0 +1,48 @@
+package WishList;
+
+import WishList.model.Wish;
+import WishList.model.Wishlist;
+import WishList.repository.WishlistJDBC;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class) // extender JUnit
+public class WishlistJDBCIT {
+    @Autowired
+    private WishlistJDBC repository;
+   /* @MockBean
+    private DataSource dataSource;
+
+   /*@Autowired
+    public WishlistJDBCIT(WishlistJDBC repository) {
+        this.repository = repository;
+    }*/
+
+    @Test
+    public void testCreateWishList() {
+        String name = "test name";
+        String description = "test description";
+        List<Wish> wishes = new ArrayList<>();
+        Wishlist wishlist = new Wishlist(name, description, wishes);
+        int userId = 1;
+
+        repository.createWishlist(wishlist, userId);
+
+        Wishlist savedWishlist = repository.getWishlist(wishlist.getId());
+        assertThat(savedWishlist)
+                .isNotNull()
+                .extracting(Wishlist::getName, Wishlist::getDescription, Wishlist::getId)
+                .containsExactly(name, description, wishlist.getId());
+
+    }
+
+}
