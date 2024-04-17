afaaae60e688e0c9250ce144d9165cda3b2a7636
hantamil <mira0004@stud.kea.dk>
2024-04-17 22:11:16 +0200

    test der ikke virker - får error ifm beans

--- src/test/java/WishList/WishListApplicationTests.java
+++ src/test/java/WishList/WishListApplicationTests.java
@@ -1,58 +1,13 @@
 package WishList;
 
-import WishList.model.Wishlist;
-import WishList.repository.WishlistJDBC;
-import WishList.service.WishlistService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
-import org.springframework.boot.autoconfigure.jms.artemis.ArtemisProperties;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 @SpringBootTest
 class WishListApplicationTests {
-    JdbcTemplateAutoConfiguration jdbcTemplate;
 
-    /*@Autowired
-    private WishlistJDBC repository;
-
-    @BeforeEach
-    public void setUp() throws SQLException {
-        try (Connection conn = DriverManager.getConnection();
-             Statement stmt = conn.createStatement()) {
-            // Create necessary tables or clear existing data
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Wishlists (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30), description VARCHAR(255), user_id INT)");
-            stmt.executeUpdate("DELETE FROM Wishlists");
-        }
-    }*/
-
-    @Test //hvad gør den her?
+    @Test //tester om applikationen virker
     void contextLoads() {
     }
-/*
-    @Test
-    void createWishListTest() {
-        Wishlist wishlist = new Wishlist("name", "description");
-        int userId = 1;
-
-        repository.createWishlist(wishlist, userId);
-
-        Wishlist savedInDatabase = repository.ge
-
-    }*/
-
-
 
 }
--- src/test/java/WishList/WishlistJDBCIT.java
+++ src/test/java/WishList/WishlistJDBCIT.java
@@ -6,9 +6,12 @@ import WishList.repository.WishlistJDBC;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,9 +19,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class) // extender JUnit
+@SpringJUnitConfig
 public class WishlistJDBCIT {
     @Autowired
     private WishlistJDBC repository;
+
+    //@Value("${spring.datasource.url}")
+    private final String db_url = "jdbc:mysql://localhost:3306/WishlistDB";
+    //@Value("${spring.datasource.username}")
+    private String username = "root";
+   // @Value("${spring.datasource.password}")
+    private String pw = "-mads18B";
+
+    @Test
+    public void getWishById() {
+        int id = 1;
+        Wish found = repository.getWishById(id);
+        assertThat(found).isNotNull();
+        assertThat(found.getId()).isEqualTo(id);
+    }
    /* @MockBean
     private DataSource dataSource;
 
@@ -27,8 +46,8 @@ public class WishlistJDBCIT {
         this.repository = repository;
     }*/
 
-    @Test
-    public void testCreateWishList() {
+   /* @Test
+    public void testCreateWishList() { //HVORFOR ER ALT NULL LDWKJHFBWELSDKHFB
         String name = "test name";
         String description = "test description";
         List<Wish> wishes = new ArrayList<>();
@@ -43,6 +62,48 @@ public class WishlistJDBCIT {
                 .extracting(Wishlist::getName, Wishlist::getDescription, Wishlist::getId)
                 .containsExactly(name, description, wishlist.getId());
 
+    }*/
+
+    private int getWishIdFromDB(Wish wish) {
+        int wishId;
+        try (Connection con = DriverManager.getConnection(db_url, username, pw)){
+            String SQL =
+                    "SELECT id FROM Wish " +
+                            "WHERE name = ? " +
+                            "AND description = ? " +
+                            "AND price = ? " +
+                            "AND url = ? " +
+                            "AND wishlist_id = ?";
+            PreparedStatement preparedStatement = con.prepareStatement(SQL);
+            preparedStatement.setString(1, wish.getName());
+            preparedStatement.setString(2, wish.getDescription());
+            preparedStatement.setDouble(3, wish.getPrice());
+            preparedStatement.setString(4, wish.getUrl());
+            preparedStatement.setInt(5, wish.getId()); // Assuming you have a method to get the wishlist ID in your Wish class
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                wishId = resultSet.getInt("id");
+            } else {
+                throw new RuntimeException("");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return wishId;
+    }
+
+    @Test
+    public void testGetWishById() {
+        Wish testWish = new Wish("Temporary Wish", "Temporary Description", 50.0, "https://ønskelink.com");
+        int testWishId = getWishIdFromDB(testWish);
+        repository.insertWish(testWish, testWishId);
+
+        Wish retrievedWish = repository.getWishById(testWishId);
+
+        assertThat(retrievedWish)
+                .isNotNull()
+                .extracting(Wish::getName, Wish::getDescription, Wish::getPrice, Wish::getUrl)
+                .containsExactly(testWish.getName(), testWish.getDescription(), testWish.getPrice(), testWish.getUrl());
     }
 
 }
